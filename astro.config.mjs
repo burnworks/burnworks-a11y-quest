ca24001fb0dcd10933be1a376e420d0c4d911e8f
burnworks <6436833+burnworks@users.noreply.github.com>
2026-04-03 20:38:13 +0900

    publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://burnworks.github.io',
+  base: '/burnworks-a11y-quest',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
